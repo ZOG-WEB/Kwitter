8acbe7c9f2735713aeb6f1a21ae45d3d52c230c8
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2021-12-03 19:24:54 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -42,3 +42,15 @@ function redirecttoRoomName(name){
       localStorage.setItem("room_name", name);
       window.location="kwitter_page.html";
 }
+
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="kwitter.html";
+}
+
+
+
+
+
+
